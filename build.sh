0d76e37e34e145fa837e6e9d46de3c866af71d62
Hoon Yang <ssang276@hanmail.net>
2020-06-15 14:40:24 +0900

    support multiple chart indexing

--- build.sh
+++ build.sh
@@ -5,11 +5,13 @@ URL=https://yanghoon.github.io/charts
 rm -rf bin/*
 
 # Build Chart
-repo=https://github.com/yanghoon/argo-app.git
-chart=$(basename $repo .git)
-
-git clone $repo bin/$chart
-helm package bin/$chart -d docs
+cat chart-repo.txt | while read repo; do
+#   repo=https://github.com/yanghoon/argo-app.git
+  chart=$(basename $repo .git)
+  
+  git clone $repo bin/$chart
+  helm package bin/$chart -d docs
+done
 
 # Indexing
 helm repo index docs --url $URL
